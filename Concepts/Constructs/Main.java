2a0df0938585b0d5b65fac7717c24bfa730e1ea0
pelfa145 <cymig213@gmail.com>
2026-08-06 05:45:30 +0800

    constructors

---
+++ Concepts/Constructs/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+   public static void main(String[] args) {
+    Student student1 = new Student("Jadon Cyrus Paran", 18,2.2);
+    Student student2 = new Student("Sheenamay Gariando", 19, 2.0);
+    System.out.println(student1.name);
+       System.out.println(student2.name+ student2.age);
+   }
+}
---
+++ Concepts/Constructs/Student.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Student{
+
+    String name;
+    int age;
+    double gpa;
+    boolean isEnrolled;
+
+    Student(String name, int age, double gpa){
+       this.name = name;
+       this.age = age;
+       this.gpa = gpa;
+
+    }
+}
