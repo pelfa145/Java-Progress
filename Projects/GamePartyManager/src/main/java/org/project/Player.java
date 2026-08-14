4e02033e12204f706172e59e389ef4b45ccc5548
pelfa145 <cymig213@gmail.com>
2026-08-15 01:07:22 +0800

    tonights stuff

--- Projects/GamePartyManager/src/main/java/org/project/Player.java
+++ Projects/GamePartyManager/src/main/java/org/project/Player.java
@@ -23,14 +23,17 @@ public class Player {
         this.health -= amount;
         System.out.println(this.username+" took "+amount+" Damage! Remaining HP: "+this.health);
     }
+
     void heal(int amount){
         this.health += amount;
         System.out.println(this.username+" has healed +"+amount+"HP.");
     }
+
     void levelUp(){
         this.level ++;
         System.out.println(this.username+" has leveled up!");
     }
+
     void displayStats(){
         System.out.println(this.username+"\nHP: "+this.health+"\nLevel: "+this.level);
     }
