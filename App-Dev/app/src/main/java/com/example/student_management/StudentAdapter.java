5c95d33444fb9d9b8b477e7ef33ca3532361b367
pelfa145 <cymig213@gmail.com>
2026-09-11 20:32:55 +0800

    complete studentadapter plspls

---
+++ App-Dev/app/src/main/java/com/example/student_management/StudentAdapter.java
@@ -0,0 +1,21 @@
+package com.example.student_management;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.StudentViewHolder> {
+
+    public class StudentViewHolder extends RecyclerView.ViewHolder{
+        public TextView studentName;
+        public TextView studentDetails;
+        public TextView studentStatus;
+        public StudentViewHolder(@NonNull View itemView) {
+            super(itemView);
+            studentName = itemView.findViewById(R.id.tvStudentName);
+            studentDetails = itemView.findViewById(R.id.tvStudentDetails);
+            studentStatus = itemView.findViewById(R.id.tvStudentStatus);
+        }
+
+    }
+}
